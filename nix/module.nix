9d030998b834b0eadb21ef88da77f542590ec0b4
foldu <foldu@protonmail.com>
2023-08-26 17:01:00 +0200

    Fix module

--- nix/module.nix
+++ nix/module.nix
@@ -3,47 +3,45 @@ let
   cfg = config.services.shutdown-thing;
 in
 {
-  services.shutdown-thing = {
-    options = with lib; {
-      enable = lib.mkEnableOption "shutdown-thing";
-      openFirewall = lib.mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Opens firewall.
-        '';
-      };
-      addr = lib.mkOptions {
-        type = types.string;
-        default = "0.0.0.0:5154";
-        description = ''
-          Addr to run shutdown-thing on.
-        '';
-      };
+  options = with lib; {
+    enable = lib.mkEnableOption "shutdown-thing";
+    openFirewall = lib.mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Opens firewall.
+      '';
     };
-    config = lib.mkIf (cfg.enable) {
-      users.users.shutdown-thing.isSystemUser = true;
-      users.groups.shutdown-thing = { };
+    addr = lib.mkOptions {
+      type = types.string;
+      default = "0.0.0.0:5154";
+      description = ''
+        Addr to run shutdown-thing on.
+      '';
+    };
+  };
+  config = lib.mkIf (cfg.enable) {
+    users.users.shutdown-thing.isSystemUser = true;
+    users.groups.shutdown-thing = { };
 
-      security.sudo.extraRules = [{
-        users = [ "shutdown-thing" ];
-        commands = builtins.flip builtins.map [ "poweroff" "reboot" "suspend" ] (cmd: {
-          command = "${pkgs.systemd}/bin/systemctl ${cmd}";
-          options = [ "NOPASSWD" ];
-        });
-      }];
+    security.sudo.extraRules = [{
+      users = [ "shutdown-thing" ];
+      commands = builtins.flip builtins.map [ "poweroff" "reboot" "suspend" ] (cmd: {
+        command = "${pkgs.systemd}/bin/systemctl ${cmd}";
+        options = [ "NOPASSWD" ];
+      });
+    }];
 
-      systemd.services.shutdown-thing = {
-        enable = true;
-        description = "Shuts things down.";
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "simple";
-          User = "shutdown-thing";
-          Group = "shutdown-thing";
-          Environment = "HOST=${cfg.addr}";
-          ExecStart = "${self.packages.${pkgs.system}.shutdown-thing}/bin/shutdown-thing";
-        };
+    systemd.services.shutdown-thing = {
+      enable = true;
+      description = "Shuts things down.";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        User = "shutdown-thing";
+        Group = "shutdown-thing";
+        Environment = "HOST=${cfg.addr}";
+        ExecStart = "${self.packages.${pkgs.system}.shutdown-thing}/bin/shutdown-thing";
       };
     };
   };
