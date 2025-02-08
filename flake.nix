67c623d9276b428ebd185293b2860ab96cabd922
foldu <foldu@protonmail.com>
2025-02-08 20:59:45 +0100

    Fix flake build

--- flake.nix
+++ flake.nix
@@ -4,25 +4,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane }: {
-    nixosModules.shutdown-thing = import ./nix/module.nix { inherit self nixpkgs; };
-  } // flake-utils.lib.eachDefaultSystem
-    (
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      crane,
+    }:
+    {
+      nixosModules.shutdown-thing = import ./nix/module.nix { inherit self nixpkgs; };
+    }
+    // flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+
         shutdown-thing =
           let
             cargoToml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
             version = cargoToml.package.version;
             pname = cargoToml.package.name;
-            craneLib = crane.lib.${system};
           in
           craneLib.buildPackage {
             inherit pname version;
